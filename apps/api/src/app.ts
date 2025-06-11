32777ce4ddb1613140cbaccea00f17090cc460b7
hermangoncalves <hermangoncalves@outlook.com>
2025-06-10 23:11:26 -0100

    Update dependencies and enhance API documentation with Scalar integration

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,8 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
 import { pinoLogger } from 'hono-pino'
-import { pinoLoggerOptions } from './utils/logger'
+import { pinoLoggerOptions } from '@/utils/logger'
+import pkgJson from '../package.json';
+import { Scalar } from '@scalar/hono-api-reference'
 
 export function createRouter() {
     const router = new OpenAPIHono({
@@ -18,6 +20,20 @@ export default function createApp() {
         http: {
             reqId: () => crypto.randomUUID(),
         },
+    }))
+
+    app.doc('/docs', {
+        openapi: '3.0.0',
+        info: {
+            title: pkgJson.name,
+            version: pkgJson.version,
+        },
+    });
+
+    app.get('/documentation', Scalar({
+        url: '/docs',
+        theme: 'kepler',
+        layout: 'modern'
     }))
 
     return app
