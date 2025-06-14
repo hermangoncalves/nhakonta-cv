279d5638276d9704f965a7f460557d07a1876608
hermangoncalves <hermangoncalves@outlook.com>
2025-06-14 18:39:32 -0100

    Refactor Discord notification handling to use sendDiscordNotification function for improved consistency. Update listLatestUsersAvatars handler to await notification sending, enhancing reliability of user notifications.

--- apps/api/src/modules/users/users.handlers.ts
+++ apps/api/src/modules/users/users.handlers.ts
@@ -5,7 +5,7 @@ import getDB from "@/db";
 import { users } from "@/db/schemas";
 import { createClerkClient } from "@clerk/clerk-sdk-node";
 import { count, desc } from "drizzle-orm";
-import { notifyDiscord } from "@/utils/notifications";
+import { notifyDiscord, sendDiscordNotification } from "@/utils/notifications";
 
 export const createUser: AppRouteHandler<CreateUserRoute> = async (c) => {
     const { clerkId } = c.get('user')
@@ -65,7 +65,7 @@ export const listLatestUsersAvatars: AppRouteHandler<LastesUsersAvatarRoute> = a
 
     const [{ count: usersCount }] = await db.select({ count: count() }).from(users)
 
-    notifyDiscord(c, {
+    await sendDiscordNotification(c, {
         title: "Listagem de Usuários",
         description: `Listagem de avatares de usuários realizada com sucesso. Total de usuários: ${usersCount}`,
         status: "success",
